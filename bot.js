c91be2a281fb690eb48bc079d23c545e8d987dee
gjhonic <gjhonic@gmail.com>
2021-01-23 01:15:02 +0800

    create new function for send message

--- bot.js
+++ bot.js
@@ -1,7 +1,24 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 let MyAuth = require('./auth_data');
+let DataSayBot = require('./data');
 let auth_data = new MyAuth();
+
+
+//Функция обработки сообщений
+function processFind(message){
+    let str_query_pos = message.content.indexOf("Данеев найди") + 13;
+    let query = message.content.slice(str_query_pos);
+
+    message.channel.send('Я не буду искать "' + query + '"');
+}
+
+
+//Обработчики событий --->
+bot.on('message', message => {
+
+    if(message.content.includes("Данеев найди")) processFind(message);
+})
 
 bot.on('ready', () => {
     console.log('Bot started ' + bot.user.tag);
---
+++ data.js
@@ -0,0 +1,9 @@
+class DataSayBot{
+  rexExpQues = [
+    "Данеев что такое",
+    "Данеев найди",
+  ]
+
+}
+
+module.exports = DataSayBot;
